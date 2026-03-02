4867f429eef21cee9ec653c2d24d1076ddf422be
Sachin <sachinprojects2@gmail.com>
2026-03-02 11:20:42 +0530

    UC6: Queue + Stack Based Palindrome Check

--- PalindromeChecker/src/PalindromeChecker.java
+++ PalindromeChecker/src/PalindromeChecker.java
@@ -1,16 +1,28 @@
-class UseCase2PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String str = "madam";
-        String reversed = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+class UseCase6PalindromeCheckerApp{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        Queue<Character> queue=new LinkedList<>();
+        Stack<Character> stack=new Stack<>();
+        for(char c:input.toCharArray()){
+            queue.add(c);
+            stack.push(c);
         }
-
-        if (str.equals(reversed)) {
-            System.out.println(str + " is a Palindrome");
-        } else {
-            System.out.println(str + " is not a Palindrome");
+        boolean isPalindrome=true;
+        while(!queue.isEmpty()){
+            if(queue.remove()!=stack.pop()){
+                isPalindrome=false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
         }
     }
 }
